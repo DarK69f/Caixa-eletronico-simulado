89f39968728ef5d4b02914fdc81055f2667ceb50
DarK69f <Pedrocastrotuti@outlook.com>
2025-07-01 13:55:32 -0300

    1.2

--- Projeto Caixa Eletronico.cpp
+++ Projeto Caixa Eletronico.cpp
@@ -6,13 +6,15 @@
 
 int main(){
 	
-	system("color 0D");	
+	system("color 0B");	
 	setlocale(LC_ALL, "Portuguese_Brazil.1252");
 
 float saldo= 2500.90, valor;
 int opcao;
 
 do{
+	system("cls");
+	
 	printf("============================================\n");
 	printf("          $ CAIXA ELETRÔNICO $  \n");
 	printf("============================================\n");
@@ -27,12 +29,14 @@ do{
 		
 		case 1:
 			printf("Seu saldo é de: R$ %.2f\n", saldo);
+			printf("Pressione ENTER para continuar...\n");
+			getchar(); getchar();
 			break;
 			
 		case 2:
 			printf("Quanto você deseja depositar?\n");
 			scanf("%f", &valor);
-			printf("Processando");
+			printf("Depositando");
 			Sleep(800); printf(".");
 			Sleep(800); printf(".");
 			Sleep(800); printf(".\n");
@@ -41,6 +45,9 @@ do{
 			
 			saldo += valor;
 			
+			printf("Seu saldo agora é de: R$ %.2f\n", saldo);
+			printf("Pressione ENTER para continuar...\n");
+			getchar(); getchar();
 			break;
 			
 		case 3:
@@ -49,15 +56,26 @@ do{
 			} else {
 			printf("Quanto você deseja sacar?\n");
 			scanf("%f", &valor);
-			printf("Processando");
+			
+			if (valor > saldo) {
+				printf("Você não possui saldo suficiente para realizar esse saque!\n");
+			} else if (valor <= 0) {
+				printf("Valor inválido!\n");
+			} else {
+				
+			printf("Sacando");
 			Sleep(1000); printf(".");
 			Sleep(1000); printf(".");
 			Sleep(500); printf(".\n");
 			Sleep(250); printf("Saque realizado com sucesso!\n");
 			
 			saldo -= valor;
+			}
 		}
 			
+			printf("Seu saldo agora é de: R$ %.2f\n", saldo);
+			printf("Pressione ENTER para continuar...\n");
+			getchar(); getchar();
 			break;
 			
 		case 4:
